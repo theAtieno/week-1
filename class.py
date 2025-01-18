f41f69cc86aeb51eb9680b67acbb658ae27dfa22
theAtieno <mjacinta073@gmail.com>
2025-01-18 11:02:05 +0300

    first commit

---
+++ class.py
@@ -0,0 +1,20 @@
+#question 1
+a = input('enter number here:')
+b = input('enter number here:')
+#this allows you to enter any random number and do the arithmetic
+
+
+print(int(a)*int(b))
+print(int(a)-int(b))
+print(int(a)+int(b))
+print(int(a)/int(b))
+
+
+#question 3
+import math
+number = 225
+result = math.sqrt(number)
+print(result)
+
+#question 2
+import random
